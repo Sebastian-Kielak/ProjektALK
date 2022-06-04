38cab28049715221d3525edbcd0492c482cfd4a1
Sebastian-Kielak <sebastian.kielak.97@gmail.com>
2022-06-04 14:23:38 +0200

    Zmiana użytej przegląðarki z Chrome na Firefox (pod Jenkinsa)

--- src/test/java/TestBasicScenario.java
+++ src/test/java/TestBasicScenario.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -40,8 +41,8 @@ public class TestBasicScenario {
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
